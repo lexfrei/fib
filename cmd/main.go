c64081e05a5dfbf0cbb1b2b624553dd3c9e438b7
Aleksey Sviridkin <f@lex.la>
2018-05-15 19:42:24 +0300

    Add main

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/lexfrei/fib/fib"
+)
+
+var n int
+
+func init() {
+	flag.IntVar(&n, "n", 0, "Fib num")
+	flag.Parse()
+}
+
+func main() {
+	c, err := fib.Fib(n)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d is %d", n, c)
+}
